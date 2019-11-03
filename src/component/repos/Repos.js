ca87beebfb249aeff405e2e0f038f36c01e68129
Amine Mouad <aminemouad1998@gmail.com>
2019-11-03 17:48:32 +0100

    Added component folder with three components files Home / Repos / ReposItems

---
+++ src/component/repos/Repos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReposItem from './ReposItem';
+
+const Repos = () => {
+  return (
+    <div>
+      <h1>Trending Repos</h1>
+      <ReposItem />
+    </div>
+  );
+};
+
+export default Repos;
---
+++ src/component/repos/ReposItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReposItem = () => {
+  return (
+    <div>
+      <h3>repos item</h3>
+    </div>
+  );
+};
+
+export default ReposItem;
